161eabf5b03e9c968e6a0622a35cc621b8d520f4
Abner Souza <100322732+AbnerSouza2@users.noreply.github.com>
2022-11-03 21:52:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function carpastel (){
+    document.getElementsByClassName ('zoom-2').onmouseover ='Pasteis';
+}
+
+function reset(){
+    document.getElementsByClassName('zoom-2').innerHTML = "";
+}
